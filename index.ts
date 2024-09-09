f4268a7c524fa8deb87ba27eded2bdc206efcdb6
humeiraiqbal <humeiraiqbal1@gmail.com>
2024-09-10 04:24:46 +0500

    Add files via upload
    Pushing code to github

---
+++ index.ts
@@ -0,0 +1,82 @@
+#! /usr/bin/env node
+let myPin =8880;
+let myBalance= 20000;
+let myName = "Humeira Iqbal"
+
+console.log(`Welcome ${myName}`);
+
+import inquirer from "inquirer";
+
+let pinAnswer= await inquirer.prompt(
+    [
+            {
+
+                name: "pin",
+                message: "please enter your pin",
+                type: "number",
+
+            }
+           
+    ]
+);
+
+if(pinAnswer.pin === myPin){
+
+console.log("Your Pin is correct")
+
+let operationAns =await inquirer.prompt(
+        [
+            {
+
+                name: "operation",
+                message: "plz select option",
+                type: "list",
+                choices:["check balance","fast cash", "withdraw"]
+  
+            }
+        ]
+    );
+    if (operationAns.operation === "withdraw"){
+
+        let amountAns = await inquirer.prompt(
+            [
+                {
+     
+                 name: "amount",
+                 message:"enter your amount",
+                 type: "number"
+     
+                }
+            ]
+    );
+    if (operationAns.operation === "fast cash"){
+
+        let amountAns = await inquirer.prompt(
+            [
+                {
+     
+                 name: "amount",
+                 message:"enter your amount",
+                 type: "number"
+     
+                }
+            ]
+    );
+    myBalance-= amountAns.amount;
+    (console.log(`your remaining balance is: ${myBalance}`));
+} 
+   
+else if(operationAns.operation === "check balance"){
+
+  console.log(`your remaining balance is: ${myBalance}`)
+
+
+} 
+
+
+
+
+
+
+}else{console.log("Incorrect Pin number")}
+}
